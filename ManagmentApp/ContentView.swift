abd6fc923434e1cd366bbdac0be56c60c2ea3d8b
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-06-19 14:05:53 -0600

    Initial Commit

---
+++ ManagmentApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ManagmentApp
+//
+//  Created by Montserrat Gomez on 2024-06-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ManagmentApp/ManagmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManagmentAppApp.swift
+//  ManagmentApp
+//
+//  Created by Montserrat Gomez on 2024-06-19.
+//
+
+import SwiftUI
+
+@main
+struct ManagmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
